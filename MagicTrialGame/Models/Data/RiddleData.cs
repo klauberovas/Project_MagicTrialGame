7e7038db1f9eb098b89c834c9cc62462421d5c39
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-07-07 11:48:34 +0200

    feat: change RiddleData.cs to record

--- MagicTrialGame/Models/Data/RiddleData.cs
+++ MagicTrialGame/Models/Data/RiddleData.cs
@@ -1,15 +1,14 @@
 namespace MagicTrialGame.Models
 {
-    public class RiddleData
-    {
-        public int Id { get; set; }
-        public int RoomNumber { get; set; }
-        public string RoomName { get; set; }
-        public string Question { get; set; }
-        public List<string> Options { get; set; }
-        public string Answer { get; set; }
-        public string Artifact { get; set; }
-        public int MagicPower { get; set; }
-        public string Hint { get; set; }
-    }
+    public record RiddleData(
+        int Id,
+        int RoomNumber,
+        string RoomName,
+        string Question,
+        List<string> Options,
+        string Answer,
+        string Artifact,
+        int MagicPower,
+        string Hint
+    );
 }
